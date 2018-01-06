29d47a82be89ec4b4d9530c728e3b94812aee521
Alexander Yakovlev <job.yakovlev.alexander@gmail.com>
2018-01-06 14:35:08 +0700

    fix select tree practice

--- __tests__/sequences.tree.test.js
+++ __tests__/sequences.tree.test.js
@@ -1,13 +1,6 @@
-import { l, length } from 'hexlet-pairs-data'; // eslint-disable-line
+import { l, length } from 'hexlet-pairs-data';
 import { make, append, node, toString as htmlToString } from 'hexlet-html-tags'; // eslint-disable-line
-import { sequencesTask } from '../src/sequences';
-import { listToString } from '../src/sequences/tree';
-
-const { select } = sequencesTask;
-
-/*
-
-*/
+import select from '../src/sequences/tree';
 
 describe('dom', () => {
   let dom;
@@ -16,6 +9,7 @@ describe('dom', () => {
     const dom1 = make();
     const dom2 = append(dom1, node('h1', 'scheme'));
     const dom3 = append(dom2, node('p', 'is a lisp'));
+
     const children1 = l(node('li', 'item 1'), node('li', 'item 2'));
     const dom4 = append(dom3, node('p', l(node('ul', children1))));
     const children2 = l(node('li', 'item 1'), node('li', 'item 2'));
@@ -25,18 +19,18 @@ describe('dom', () => {
     const dom7 = append(dom6, node('div', l(node('p', l(node('ul', children3))))));
     const dom8 = append(dom7, node('div', l(node('p', 'another text'))));
     const dom9 = append(dom8, node('div', l(node('div', l(node('p', l(node('span', 'text'))))))));
-    const dom10 = append(dom9, node('div', l(node('a', l(node('div', l(node('p', l(node('span', 'text'))))))))));
-    const dom11 = append(dom10, node('h1', 'prolog'));
-    dom = append(dom11, node('p', 'is about logic'));
+
+    const dom10 = append(dom9, node('h1', 'prolog'));
+    dom = append(dom10, node('p', 'is about logic'));
   });
 
   it('#select', () => {
-    console.log(listToString(select(l('p', 'ul', 'li'), dom)));
-    expect(length(select(l('p', 'ul', 'li'), dom))).toBe(2);
-    // expect(length(select(l('div', 'div', 'p'), dom))).toBe(1);
-    // expect(length(select(l('div', 'p'), dom))).toBe(2);
-    // expect(length(select(l('p'), dom))).toBe(8);
-    // expect(length(select(l('ul'), dom))).toBe(2);
-    // expect(length(select(l('div'), dom))).toBe(4);
+    // console.log(htmlToString(select(l('p', 'ul', 'li'), dom)));
+    expect(length(select(l('p', 'ul', 'li'), dom))).toBe(3);
+    expect(length(select(l('div', 'div', 'p'), dom))).toBe(1);
+    expect(length(select(l('div', 'p'), dom))).toBe(3);
+    expect(length(select(l('p'), dom))).toBe(7);
+    expect(length(select(l('ul'), dom))).toBe(2);
+    expect(length(select(l('div'), dom))).toBe(3);
   });
 });
--- src/sequences/tree.js
+++ src/sequences/tree.js
@@ -3,76 +3,28 @@ import { l, isList, isEmpty, head, tail, concat, toString as listToString } from
 // eslint-disable-next-line
 import { is, toString as htmlToString, hasChildren, children, filter, reduce } from 'hexlet-html-tags';
 
-const select = (query, dom) => {
-  const getStartTags = (startTag, dom2) => {
-    const filtered = filter(element => is(startTag, element), dom2);
-    if (startTag === 'div') return filtered;
-    return reduce((element, acc) => {
-      if (hasChildren(element)) {
-        return concat(acc, getStartTags(startTag, children(element)));
-      }
-      return acc;
-    }, filtered, dom2);
-  };
-
-  const getFirstChildren = (tree) => {
-    if (isList(tree)) {
-      const ans = reduce((element, acc) => {
-        if (isEmpty(acc)) {
-          if (isList(element)) return l(element);
-          return l(element);
-        }
-        return acc;
-      }, l(), tree);
-      return ans;
+const select = (query, tree) => {
+  const selectMatch = (innerQuery, dom) => {
+    if (isEmpty(innerQuery)) {
+      return dom;
     }
-    return l(tree);
-  };
-
-  const getMatches = (rest, dom2) => {
-    if (isEmpty(rest)) {
-      return dom2;
-    }
-    const isLastQuery = isEmpty(tail(rest));
-    const compareTag = head(rest);
+    const isLastQuery = isEmpty(tail(innerQuery));
+    const compareTag = head(innerQuery);
     const matchedElements = reduce((element, acc) => {
+      const nested = hasChildren(element) ? selectMatch(query, children(element)) : l();
       if (is(compareTag, element)) {
         if (isLastQuery) {
           return concat(acc, l(element));
         }
         if (hasChildren(element)) {
-          const firstChildren = getFirstChildren(children(element));
-          const first = isList(firstChildren) ? firstChildren : l(firstChildren);
-          return concat(first, acc);
+          return concat(nested, concat(acc, selectMatch(tail(innerQuery), children(element))));
         }
       }
-      return acc;
-    }, l(), dom2);
-    return getMatches(tail(rest), matchedElements);
+      return concat(acc, nested);
+    }, l(), dom);
+    return matchedElements;
   };
-  const startDom = getStartTags(head(query), dom);
-  const answer = getMatches(query, startDom);
-  return answer;
+  return selectMatch(query, tree);
 };
 
-// const select = (query, html, insideQuery = false) => {
-//   if (isEmpty(query)) {
-//     return html;
-//   }
-//   const newHtml = reduce((element, acc) => {
-//     if (is(head(query), element)) {
-//       const childrenElements = hasChildren(element) ? children(element) : l();
-//       const elements = isEmpty(tail(query)) ? l(element) : childrenElements;
-//       return concat(elements, acc);
-//     }
-//     if (hasChildren(element) && !insideQuery) {
-//       return concat(select(query, children(element)), acc);
-//     }
-//     return acc;
-//   }, l(), html);
-//   const ans = select(tail(query), newHtml, true);
-//   // console.log(listToString(ans));
-//   return select(tail(query), newHtml, true);
-// };
-export { listToString };
 export default select;
