3687a22417393f1f1ea5e5d2cfbae6abc491c430
Alexander Yakovlev <job.yakovlev.alexander@gmail.com>
2018-04-04 23:15:16 +0700

    fix lint errors

--- __tests__/protoypes.ast.test.js
+++ __tests__/protoypes.ast.test.js
@@ -17,7 +17,7 @@ describe('HtmlBuilder', () => {
     ]];
 
     const actual = render(parse(data));
-    const expected = `<html><head><title>hello, hexlet!</title></head><body class="container"><h1 class="header">html builder example</h1><div><span>span text2</span><br></div></body></html>`;
+    const expected = '<html><head><title>hello, hexlet!</title></head><body class="container"><h1 class="header">html builder example</h1><div><span>span text2</span><br></div></body></html>';
     expect(actual).toBe(expected);
   });
 
@@ -35,7 +35,7 @@ describe('HtmlBuilder', () => {
     ]];
 
     const actual = render(parse(data));
-    const expected = `<html><body><h2 class="header">first header</h2><img class="picture" src="smile.jpg"><div><p>hello, world</p><p>good bye, world</p><a class="link" href="https://hexlet.io">hexlet.io</a></div></body></html>`;
+    const expected = '<html><body><h2 class="header">first header</h2><img class="picture" src="smile.jpg"><div><p>hello, world</p><p>good bye, world</p><a class="link" href="https://hexlet.io">hexlet.io</a></div></body></html>';
     expect(actual).toBe(expected);
   });
 });
