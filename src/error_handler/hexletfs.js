fe8b4ac463198255e945b3b999be3a4cd3dc232e
Alexander Yakovlev <job.yakovlev.alexander@gmail.com>
2018-05-02 21:08:48 +0700

    add fs practice

---
+++ src/error_handler/hexletfs.js
@@ -0,0 +1,36 @@
+import path from 'path';
+import Tree from './tree'; // eslint-disable-line
+
+const getPathParts = filepath =>
+  filepath.split(path.sep).filter(x => x);
+
+export default class {
+  constructor() {
+    this.tree = new Tree('/', { type: 'dir' });
+  }
+
+  isDirectory(filepath) {
+    const current = this.findNode(filepath);
+    return current && current.getMeta().type === 'dir';
+  }
+
+  isFile(filepath) {
+    const current = this.findNode(filepath);
+    return current && current.getMeta().type === 'file';
+  }
+
+  mkdirSync(filepath) {
+    const { dir, base } = path.parse(filepath);
+    this.findNode(dir).addChild(base, { type: 'dir' });
+  }
+
+  touchSync(filepath) {
+    const { dir, base } = path.parse(filepath);
+    this.findNode(dir).addChild(base, { type: 'file' });
+  }
+
+  findNode(filepath) {
+    const parts = getPathParts(filepath);
+    return parts.length === 0 ? this.tree : this.tree.getDeepChild(parts);
+  }
+}
